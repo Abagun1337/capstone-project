49bfdea83d4ed73e237583ed24ca67f83f21ff26
Abuja <you@Abaguntoyosi002@gmail.com>
2026-06-09 01:25:55 +0100

    fix: move Base above engine in db_async

--- app/core/db_async.py
+++ app/core/db_async.py
@@ -2,15 +2,13 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-DATABASE_URL = settings.DATABASE_URL_ASYNC
+
+class Base(DeclarativeBase):
+    pass
 
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL_ASYNC, echo=True)
 
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
-
-
-class Base(DeclarativeBase):
-    pass
